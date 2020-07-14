71e5b668628865c48877913e83212f8ac7e9bddb
Matheus Morais <morais629@gmail.com>
2020-07-14 18:01:25 -0300

    Carregando produtos de uma Categoria

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -26,5 +26,7 @@ export class CategoriasPage {
       error => {});
   }
 
-
+  showProdutos(categoria_id : string) {
+    this.navCtrl.push('ProdutosPage', {categoria_id: categoria_id});   
+  }
 }
