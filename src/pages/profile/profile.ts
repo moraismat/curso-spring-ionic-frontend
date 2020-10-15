4d4f7753a48d195fe4088a74afcdda702e331136
Matheus Araujo de Morais <matheus.morais@codata.pb.gov.br>
2020-10-15 15:59:55 -0300

    Buscando endereços no banco de dados

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -26,7 +26,7 @@ export class ProfilePage {
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(res => {
-          this.cliente = res;
+          this.cliente = res as ClienteDTO;
 
         },
           error => {
