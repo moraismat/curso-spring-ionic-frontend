b9b0f0fb92c0f8c7fd192704c655843579c4b45e
Matheus Araujo de Morais <matheus.morais@codata.pb.gov.br>
2020-10-16 21:00:12 -0300

    Salvando Pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { EnderecoDTO } from '../../models/endereco.dto';
 import { PedidoDTO } from '../../models/pedido.dto';
 import { CartService } from '../../services/domain/cart.service';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 
 @IonicPage()
@@ -24,31 +25,48 @@ export class OrderConfirmationPage {
     public navCtrl: NavController,
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService) {
 
     this.pedido = this.navParams.get('pedido');
 
   }
 
   ionViewDidLoad() {
-   this.cartItems = this.cartService.getCart().items;
-   console.log(this.cartItems)
-   this.clienteService.findById(this.pedido.cliente.id)
-    .subscribe(res => {
-      this.cliente = res as ClienteDTO;
-      this.endereco = this.findEndereco(this.pedido.enderecoDeEntrega.id, res['enderecos'])
-    },
-    error =>{
-      this.navCtrl.setRoot('HomePage')
-    })
+    this.cartItems = this.cartService.getCart().items;
+    console.log(this.cartItems)
+    this.clienteService.findById(this.pedido.cliente.id)
+      .subscribe(res => {
+        this.cliente = res as ClienteDTO;
+        this.endereco = this.findEndereco(this.pedido.enderecoDeEntrega.id, res['enderecos'])
+      },
+        error => {
+          this.navCtrl.setRoot('HomePage')
+        })
   }
 
-  private findEndereco(id: string, list: EnderecoDTO[]): EnderecoDTO{
+  private findEndereco(id: string, list: EnderecoDTO[]): EnderecoDTO {
     let position = list.findIndex(x => x.id = id)
     return list[position]
   }
 
-  total(){
+  total() {
     return this.cartService.total();
+  }
+
+  checkout() {
+    this.pedidoService.insert(this.pedido).subscribe(res => {
+      this.cartService.createOrClearCart()
+      console.log(res.headers.get('location'))
+    },
+      error => {
+        if (error.status == 403) {
+          this.navCtrl.setRoot('HomePage')
+        }
+      })
+  }
+
+  back(){
+    this.navCtrl.setRoot('CartPage')
   }
 }
